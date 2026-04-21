b3d1a0d13fc47e47cef02e00fba12d334a00b493
Chiiioo <37750496+Chiiioo@users.noreply.github.com>
2026-04-21 11:32:57 -0400

    Add Demo class with main method

---
+++ main.java
@@ -0,0 +1,7 @@
+public class Demo {
+    public static void main(String[] args) {
+        int number = 42;
+        String message = "The answer is: ";
+        System.out.println(message + number);
+    }
+}
